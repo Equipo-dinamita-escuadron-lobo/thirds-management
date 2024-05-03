7c8fd5415dbfe95038a2942fe1865bd3c194e2f5
SantiagoAV <81932949+SantiAV25@users.noreply.github.com>
2024-05-03 08:15:12 -0500

    test

--- src/main/java/com/thirdsmanagement/thirds/domain/exception/ThirdNotFound.java
+++ src/main/java/com/thirdsmanagement/thirds/domain/exception/ThirdNotFound.java
@@ -1,5 +1,5 @@
 package com.thirdsmanagement.thirds.domain.exception;
-
+/*Reset DataBase*/
 public class ThirdNotFound extends RuntimeException{
     public ThirdNotFound(String message){
         super(message);
