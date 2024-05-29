5de46a859aa0550c169f3d72e3dffdc17711fe28
Julian Ruano <113054523+JulianRuano@users.noreply.github.com>
2024-05-28 22:33:04 -0500

    Update TestController.java

--- src/main/java/com/thirdsmanagement/thirds/infrastructure/adapters/input/rest/TestController.java
+++ src/main/java/com/thirdsmanagement/thirds/infrastructure/adapters/input/rest/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/api/categories/test")
+@RequestMapping("/api/thirds/test")
 public class TestController {
     @GetMapping("/ping")
     public String ping() {
