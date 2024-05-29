a7c49461fafd273d5be56f623a5cbc456b90ef72
JulianRuano <julianrmajin@gmail.com>
2024-05-28 22:21:24 -0500

    Add TestController for ping endpoint

---
+++ src/main/java/com/thirdsmanagement/thirds/infrastructure/adapters/input/rest/TestController.java
@@ -0,0 +1,16 @@
+package com.thirdsmanagement.thirds.infrastructure.adapters.input.rest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/categories/test")
+public class TestController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
