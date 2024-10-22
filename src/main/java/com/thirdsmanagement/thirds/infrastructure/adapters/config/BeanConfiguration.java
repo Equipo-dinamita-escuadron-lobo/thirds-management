fd59c550924359dabaa4b7b8cd6d497780cadbf8
cristian <cristiancdr@unicauca.edu.co>
2024-10-22 08:08:44 -0500

    borrar tipo terceros, a ver si se sube bien

--- src/main/java/com/thirdsmanagement/thirds/infrastructure/adapters/config/BeanConfiguration.java
+++ src/main/java/com/thirdsmanagement/thirds/infrastructure/adapters/config/BeanConfiguration.java
@@ -8,6 +8,7 @@ import com.thirdsmanagement.thirds.application.service.ChangeThirdStateService;
 import com.thirdsmanagement.thirds.application.service.CreateThirdService;
 import com.thirdsmanagement.thirds.application.service.CreateThirdTypeService;
 import com.thirdsmanagement.thirds.application.service.CreateTypeIdService;
+import com.thirdsmanagement.thirds.application.service.DeleteTypeThirdService;
 import com.thirdsmanagement.thirds.application.service.GetThirdService;
 import com.thirdsmanagement.thirds.application.service.ListThirdTypeService;
 import com.thirdsmanagement.thirds.application.service.ListThirdsService;
@@ -21,6 +22,7 @@ import com.thirdsmanagement.thirds.infrastructure.adapters.output.persistence.ma
 import com.thirdsmanagement.thirds.infrastructure.adapters.output.persistence.mapper.ThirdPersistenceMapper;
 import com.thirdsmanagement.thirds.infrastructure.adapters.output.persistence.repository.ThirdRepository;
 import com.thirdsmanagement.thirds.infrastructure.adapters.output.persistence.repository.ThirdTypeRepository;
+import com.thirdsmanagement.thirds.infrastructure.adapters.output.persistence.repository.ThirdsAndTypesRepository;
 import com.thirdsmanagement.thirds.infrastructure.adapters.output.persistence.repository.TypeIdRepository;
 
 @Configuration
@@ -92,5 +94,9 @@ public class BeanConfiguration {
     @Bean
     public GetThirdService getThirdService(ThirdPersistenceAdapter thirdPersistenceAdapter){
         return new GetThirdService(thirdPersistenceAdapter);
+    }
+    @Bean
+    public DeleteTypeThirdService deleteTypeThirdService(ThirdTypeRepository thirdTypeRepository, ThirdsAndTypesRepository thirdsAndTypesRepository ) {
+        return new DeleteTypeThirdService(thirdTypeRepository, thirdsAndTypesRepository);
     }
 }
