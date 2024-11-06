b7011d689bb2a8ee2ffaa599efb39ec9648bd5f2
Francisco Eraso <114776394+cfidrobo16@users.noreply.github.com>
2024-11-05 23:11:56 -0500

    Nuevo tipo de identifiacion por defecto

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,7 @@ INSERT IGNORE INTO third_type (tt_id, tt_name, tt_entid) VALUES
 (4, 'Otro','standart');
 INSERT IGNORE INTO type_id (ti_id, ti_name, ti_entid) VALUES
 ('Cédula de Ciudadanía', 'Cédula de Ciudadanía','standart'),
+('NIT', 'NIT','standart'),
 ('Cédula de Extranjería', 'Cédula de Extranjería','standart'),
 ('Número de Identificación Tributaria', 'Número de Identificación Tributaria','standart'),
 ('Pasaporte', 'Pasaporte','standart'),
