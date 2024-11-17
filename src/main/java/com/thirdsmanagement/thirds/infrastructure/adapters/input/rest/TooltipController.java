87b66379b73e231963971266dd0d4c65f4382aa0
cristian6619 <cristiancdr@unicauca.edu.co>
2024-11-17 18:54:51 -0500

    nueva url tooltip

--- src/main/java/com/thirdsmanagement/thirds/infrastructure/adapters/input/rest/TooltipController.java
+++ src/main/java/com/thirdsmanagement/thirds/infrastructure/adapters/input/rest/TooltipController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/tooltip")
+@RequestMapping("/api/thirds/tooltip")
 public class TooltipController {
 
     @Autowired
