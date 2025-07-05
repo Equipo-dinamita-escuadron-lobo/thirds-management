67ba76ec272c7b2a87411f86d82a45ed4e935253
JulianRuano <julianrmajin@gmail.com>
2025-07-05 13:52:52 -0500

    Refactor: update dependencies and configuration for PostgreSQL, add support for Eureka

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,16 @@
-INSERT IGNORE INTO third_type (tt_id, tt_name, tt_entid) VALUES
+INSERT INTO third_type (tt_id, tt_name, tt_entid) VALUES
 (1, 'Cliente','standart'),
 (2, 'Proveedor','standart'),
 (3, 'Empleado','standart'),
 (4, 'Otro','standart');
-INSERT IGNORE INTO type_id (ti_id, ti_name, ti_entid) VALUES
+INSERT INTO type_id (ti_id, ti_name, ti_entid) VALUES
 ('Cédula de Ciudadanía', 'Cédula de Ciudadanía','standart'),
 ('NIT', 'NIT','standart'),
 ('Cédula de Extranjería', 'Cédula de Extranjería','standart'),
 ('Número de Identificación Tributaria', 'Número de Identificación Tributaria','standart'),
 ('Pasaporte', 'Pasaporte','standart'),
 ('Tarjeta de Identidad', 'Tarjeta de Identidad','standart');
-INSERT IGNORE INTO tooltip (ent_Id, tip) VALUES
+INSERT INTO tooltip (ent_Id, tip) VALUES
 ('1', 'Cuadro ayuda uno'),
 ('2', 'Cuadro ayuda dos'),
 ('3', 'Cuadro ayuda tres'),
