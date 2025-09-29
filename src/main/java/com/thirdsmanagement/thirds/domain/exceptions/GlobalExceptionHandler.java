f05c123c83d1087c5d0cc3d70e0ed16845c289a8
santiagonieto09 <santiago.nieto09@gmail.com>
2025-09-29 13:45:28 -0500

    refactor: ErrorResponse A /data/response

--- src/main/java/com/thirdsmanagement/thirds/domain/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/thirdsmanagement/thirds/domain/exceptions/GlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 import com.thirdsmanagement.thirds.domain.exceptions.thirdType.ThirdTypeForeignKeyViolationException;
 import com.thirdsmanagement.thirds.domain.exceptions.typeId.TypeIdForeignKeyViolationException;
+import com.thirdsmanagement.thirds.infrastructure.adapters.input.rest.data.response.ErrorResponse;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
